ef27f2d81a834c6fbb826759f9b04bdfdfa98a4e
Lustgrow <deploy@lustgrow.com.ar>
2026-03-24 19:37:46 -0300

    Fix: listen on 0.0.0.0 for Railway

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Lustgrow ML running on port ${PORT}`);
 });
